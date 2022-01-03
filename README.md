13b76bbf676cd1e5b3399761011247585dc8809d
Nicolas Ritouet <nicolas@ritouet.com>
2022-01-03 12:05:51 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Minisite pour la campagne de recrutement de bénévoles
+
+> Landing page contenant la liste des rôles que le Club recherche et un formulaire (php) pour envoyer un message d'intéret.
+
+Pour lancer le site, il faut php:  
+`php -S 127.0.0.1:8000`
+
+puis aller a http://127.0.0.1:8000 avec votre navigateur préféré. 👍🏻
---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="présentation du bénévolat au sein du Club Alpin Francais de Lyon-Villeurbanne" />
+    <meta name="author" content="Groupe informatique du Club <numerique@clubalpinlyon.fr>" />
+    <title>Bénévolat au Club Alpin Francais de Lyon-Villeurbanne</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top"><img src="assets/img/CAF_Lyon-Villeurbanne.png" alt="..." /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#benevolat">Bénévolat</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#roles">Les rôles</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contact">Nous contacter</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://www.clubalpinlyon.fr">Retour au site</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- HERO-->
+    <header class="masthead">
+        <div class="container">
+            <div class="masthead-subheading">Les bénévoles sont au cœur de l'action.</div>
+            <div class="masthead-heading text-uppercase">C'est TON Club !</div>
+            <a class="btn btn-primary btn-xl text-uppercase" href="#services">En savoir plus</a>
+        </div>
+    </header>
+    <!-- Devenir bénévole-->
+    <section class="fdb-block">
+        <div class="container">
+          <div class="row justify-content-center pb-5">
+            <div class="col-12 text-center">
+              <h1>Devenir bénévole ?</h1>
+            </div>
+          </div>
+      
+          <div class="row text-left align-items-center pt-5 pb-md-5">
+            <div class="col-4 col-md-5">
+              <img alt="image" class="img-fluid" src="./assets/img/commitment.svg">
+            </div>
+            <div class="col-12 col-md-5 m-md-auto">
+              <h2><strong>Engagement</strong></h2>
+              <p class="lead">S’enrichir humainement en s’impliquant à la hauteur de ses disponibilités dans des actions qui ont du sens.</p>
+            </div>
+          </div>
+      
+          <div class="row text-left align-items-center pt-5 pb-md-5">
+            <div class="col-4 col-md-5 m-md-auto order-md-5">
+              <img alt="image" class="img-fluid" src="./assets/img/high-five.svg">
+            </div>
+            <div class="col-12 col-md-5">
+              <h2><strong>Des belles rencontres</strong></h2>
+              <p class="lead">Devenir bénévole, c'est rejoindre une équipe accueillante et riche de volontaires de tous bords, animés par une passion commune: la montagne.</p>
+            </div>
+          </div>
+      
+          <div class="row text-left align-items-center pt-5">
+            <div class="col-4 col-md-5">
+              <img alt="image" class="img-fluid" src="./assets/img/competences.svg">
+            </div>
+            <div class="col-12 col-md-5 m-md-auto">
+              <h2><strong>Apprendre et partager des compétences</strong></h2>
+              <p class="lead">Participer aux projets du Club nécessite de mettre en pratique vos compétences acquises dans un cadre profesionnel, étudiant ou de loisir et d'en apprendre des nouvelles. Ces compétences peuvent être à leur tour utile pour votre vie professionnelle.<br/>
+                Les projets du Club sont divers et variés. Cela va de l'accueil des nouveaux adhérents au développement d'outils informatiques en passant par la gestion et le suivi du matériel. Et ne pensez pas qu'il faille forcement avoir de l'expérience, beaucoup de rôles sont ouverts aux débutants. 
+              </p>
+            </div>
+          </div>
+
+          <div class="row text-left align-items-center pt-5 pb-md-5">
+            <div class="col-4 col-md-5 m-md-auto order-md-5">
+              <img alt="image" class="img-fluid" src="./assets/img/awesome.svg">
+            </div>
+            <div class="col-12 col-md-5">
+              <h2><strong>Des privilèges</strong></h2>
+              <p class="lead">Obtenir le statut de bénévole actif, c'est avoir accès à différents avantages tels que:<br />
+                  - réduction sur la part fédéral de l'abonnement<br />
+                  - réduction sur l'assurance annuelle<br />
+                  - réduction supplémentaire chez certains partenaires<br />
+                  - reconnaissance avec la carte de bénévoles<br />
+                  - invitation à des évenements dédiés aux bénévoles
+              </p>
+            </div>
+          </div>
+
+
+        </div>
+      </section>
+    <!-- Les rôles-->
+    <section class="page-section bg-light" id="team">
+        <div class="container">
+            <div class="text-center">
+                <h2 class="section-heading text-uppercase">Les rôles</h2>
+                <h3 class="section-subheading text-muted">Voici une liste non exhaustive des rôles qui pourraient être
+                    utiles au Club.</h3>
+            </div>
+            <div class="row">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Postuler</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Département</th>
+                            <th scope="col">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $json = file_get_contents('./js/roles.json');
+                            $obj = json_decode($json, true);
+                            foreach($obj as $key => $value): ?>
+                                <tr>
+                                    <td><input type="checkbox" /></td>
+                                    <td><?php echo $value['Nom']; ?></td>
+                                    <td><?php echo $value['Departement']; ?></td>
+                                    <td><?php echo $value['Description']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <p>
+                    <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["action"])): ?>
+                        <script>
+                            alert('Votre message a été envoyé, nous reviendrons vers vous très rapidement.');
+                        </script>
+                        <?php else: ?>
+                            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                                <input type="hidden" value="email" name="action" />
+                                <div class="row align-items-stretch mb-5">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <!-- Name input-->
+                                            <input class="form-control" id="name" type="text" placeholder="Votre nom et prénom *" />
+                                            <div class="invalid-feedback">A name is required.</div>
+                                        </div>
+                                        <div class="form-group">
+                                            <!-- Email address input-->
+                                            <input class="form-control" id="email" type="email" placeholder="Votre email *"
+                                                data-sb-validations="required,email" />
+                                            <div class="invalid-feedback">An email is required.</div>
+                                            <div class="invalid-feedback">Email is not valid.</div>
+                                        </div>
+                                        <div class="form-group mb-md-0">
+                                            <!-- Phone number input-->
+                                            <input class="form-control" id="phone" type="tel" placeholder="Votre numéro de téléphone" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group form-group-textarea mb-md-0">
+                                            <!-- Message input-->
+                                            <textarea class="form-control" id="message" placeholder="Un petit message ?"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Submit success message-->
+                                <!---->
+                                <!-- This is what your users will see when the form-->
+                                <!-- has successfully submitted-->
+                                <div class="d-none" id="submitSuccessMessage">
+                                    <div class="text-center text-white mb-3">
+                                        <div class="fw-bolder">Form submission successful!</div>
+                                        To activate this form, sign up at
+                                        <br />
+                                        <a
+                                            href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                    </div>
+                                </div>
+                                <!-- Submit error message-->
+                                <!---->
+                                <!-- This is what your users will see when there is-->
+                                <!-- an error submitting the form-->
+                                <div class="d-none" id="submitErrorMessage">
+                                    <div class="text-center text-danger mb-3">Error sending message!</div>
+                                </div>
+                                <!-- Submit Button-->
+                                <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase disabled"
+                                        id="submitButton" type="submit">Send Message</button></div>
+                            </form>
+                        <?php endif; ?><br />
+                        <a href="mailto:benevolat@clubalpinlyon.fr">benevolat@clubalpinlyon.fr</a>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <p class="large text-muted">La majorité des rôles sont accessibles aux débutants, n'hésitez pas à nous contacter pour en savoir plus sur un rôle en particulier.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 text-lg-start">Copyright &copy; Club Alpin Francais de Lyon-Villeurbanne 2022</div>
+                <div class="col-lg-4 my-3 my-lg-0">
+                    <a class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/clubalpinlyonvilleurbanne/"><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/Club-Alpin-Fran%C3%A7ais-de-Lyon-Villeurbanne-118851941629324/"><i class="fab fa-facebook-f"></i></a>
+                    <!-- <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-linkedin-in"></i></a> -->
+                </div>
+                <div class="col-lg-4 text-lg-end">
+                    <!-- <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a> -->
+                    <a class="link-light text-decoration-none" href="https://www.clubalpinlyon.fr/pages/mentions-legales.html">Mentions légales</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
